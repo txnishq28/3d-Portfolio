bdfc555653bc6b6a76ea5ddd018501967e1047fb
TANISHQ TIWARI <144956446+txnishq28@users.noreply.github.com>
2025-06-05 23:12:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import { NavLink } from "react-router-dom";
 
-
-
 const Navbar = () => {
   return (
     <header className='header'>
@@ -9,7 +7,7 @@ const Navbar = () => {
         <p className="purple-gradient_text">TT</p>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ?"text-purple-400" : "text-black" }>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-purple-400" : "text-black"}>
           About
         </NavLink>
         <NavLink to='/projects' className={({ isActive }) => isActive ? "text-purple-400" : "text-black"}>
@@ -18,6 +16,14 @@ const Navbar = () => {
         <NavLink to='/Contact' className={({ isActive }) => isActive ? "text-purple-400" : "text-black"}>
           Contact
         </NavLink>
+        <a
+          href="https://drive.google.com/file/d/1j14pwKvAVytb99zT8qUUaX3hRCE_sVX6/view?usp=drive_link"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black hover:text-purple-400"
+        >
+          Resume
+        </a>
       </nav>
     </header>
   );
